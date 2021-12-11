e4e462c606b51c4307297a88d51e9e773e0c8822
skoto <0120koto@gmail.com>
2021-12-11 18:02:59 +0900

    🎉

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import "./style/index.css";
+
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
+
+import { App } from "./App";
+
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById("root")
+);
